177ae90c978e3cd0853120ca78d840c5b2ab48b1
Harrison McCarty <hmccarty@pm.me>
2022-06-08 12:03:41 -0400

    updated gelsight configuration

--- src/image_stream.cpp
+++ src/image_stream.cpp
@@ -10,50 +10,43 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "gelsight_image_stream");
+  ros::init(argc, argv, "streamer");
 
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
  
   std::string name; 
+  int rate;
   bool publish_raw, publish_rectified, publish_mask; 
  
-  nh.getParam("name", name);
-  nh.getParam("publish_raw", publish_raw);
-  nh.getParam("publish_rectified", publish_rectified);
-  nh.getParam("publish_mask", publish_mask);
+  ros::param::get("~rate", rate);
+  ros::param::get("~publish_raw", publish_raw);
+  ros::param::get("~publish_rectified", publish_rectified);
+  ros::param::get("~publish_mask", publish_mask);
 
   cv::Point2f srcCorners[4];
-  std::map<std::string, double> top_left, top_right, bot_left, bot_right;
-  nh.getParam("image_corners/top_left", top_left);
-  srcCorners[0] = cv::Point2f((float) top_left["x"], (float) top_left["y"]);
-  srcCorners[0] = cv::Point2f(158.f, 134.f);
+  std::string corner_params[4] = { "~image_corners/top_left/", "~image_corners/top_right/",
+	                           "~image_corners/bottom_left/", "~image_corners/bottom_right/" };
+  for (int i = 0; i < 4; i++) {
+    float x, y;
+    ros::param::get(corner_params[i] + 'x', x);
+    ros::param::get(corner_params[i] + 'y', y);
+    srcCorners[i] = cv::Point2f(x, y);
+  }
 
-  nh.getParam("image_corners/top_right", top_right);
-  srcCorners[1] = cv::Point2f((float) top_right["x"], (float) top_right["y"]);
-  srcCorners[1] = cv::Point2f(505.f, 109.f);
-
-  nh.getParam("image_corners/bot_left", bot_left);
-  srcCorners[2] = cv::Point2f((float) bot_left["x"], (float) bot_left["y"]);
-  srcCorners[2] = cv::Point2f(163.f, 356.f);
-
-  nh.getParam("image_corners/bot_right", bot_right);
-  srcCorners[3] = cv::Point2f((float) bot_right["x"], (float) bot_right["y"]);
-  srcCorners[3] = cv::Point2f(506.f, 386.f);
-
-  double width, height;
-  nh.getParam("image_size/width", width);
-  nh.getParam("image_size/height", height);
+  float width, height;
+  ros::param::get("~image_size/width", width);
+  ros::param::get("~image_size/height", height);
 
   cv::Point2f dstCorners[4];
   dstCorners[0] = cv::Point2f(0.f,0.f);
-  dstCorners[1] = cv::Point2f(640.f, 0.f);
-  dstCorners[2] = cv::Point2f(0.f, 480.f);
-  dstCorners[3] = cv::Point2f(640.f, 480.f);
+  dstCorners[1] = cv::Point2f(width, 0.f);
+  dstCorners[2] = cv::Point2f(0.f, height);
+  dstCorners[3] = cv::Point2f(width, height);
 
-  image_transport::Publisher raw_image_pub = it.advertise(cv::format("%s/image/raw", name.c_str()), 1);
-  image_transport::Publisher rect_image_pub = it.advertise(cv::format("%s/image/rectified", name.c_str()), 1);
-  image_transport::Publisher mask_image_pub = it.advertise(cv::format("%s/image/mask", name.c_str()), 1);
+  image_transport::Publisher raw_image_pub = it.advertise(cv::format("image/raw", name.c_str()), 1);
+  image_transport::Publisher rect_image_pub = it.advertise(cv::format("image/rectified", name.c_str()), 1);
+  image_transport::Publisher mask_image_pub = it.advertise(cv::format("image/mask", name.c_str()), 1);
 
   cv::VideoCapture capture;
   if (!capture.open(0, cv::CAP_ANY)) {
@@ -70,7 +63,7 @@ int main(int argc, char **argv)
   cv::Mat grayscale_image;
   cv::Mat mask_image;
   
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(rate);
   while (ros::ok())
   {
     if (capture.read(raw_image)) { 
@@ -80,27 +73,23 @@ int main(int argc, char **argv)
       }
 
       cv::Mat image_tf = cv::getPerspectiveTransform(srcCorners, dstCorners);
-      cv::warpPerspective(raw_image, rect_image, image_tf, cv::Size(640.f, 480.f)); 
+      cv::warpPerspective(raw_image, rect_image, image_tf, cv::Size(width, height)); 
 
       if (publish_rectified) {
         sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", rect_image).toImageMsg();
         rect_image_pub.publish(msg);
       }
 
-      continue;
-
-      cv::cvtColor(rect_image, grayscale_image, cv::COLOR_BGR2GRAY);
-      cv::adaptiveThreshold(grayscale_image, mask_image, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 21, 10);
-
-      if (false) {
+      if (publish_mask) {
+        cv::cvtColor(rect_image, grayscale_image, cv::COLOR_BGR2GRAY);
+        cv::adaptiveThreshold(grayscale_image, mask_image, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 21, 10);
         sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", mask_image).toImageMsg();
         mask_image_pub.publish(msg);
       }
-
     } else {
       ROS_WARN("Frame dropped");
     }
-
+    
     ros::spinOnce();
     loop_rate.sleep();
   }
