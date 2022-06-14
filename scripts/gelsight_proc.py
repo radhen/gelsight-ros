ee584845020963fd2e0ab544c15f7f08f646de34
Harrison McCarty <hmccarty@pm.me>
2022-06-14 00:57:16 -0400

    fixed pca compute

--- scripts/gelsight_proc.py
+++ scripts/gelsight_proc.py
@@ -34,31 +34,33 @@ def depth2pcl(px, py, mmpp, dm):
               PointField('rgb', 12, PointField.UINT32, 1)]
 
     header = Header()
-    header.frame_id = "map"
-    
     return point_cloud2.create_cloud(header, fields, points)
 
-def pca(pcl):
-    pcl = pcl.reshape(-1, 2).astype(np.float64)
-    mv = np.mean(pcl, 0).reshape(2, 1)
-    pcl -= mv.T
-    w, v = LA.eig(np.dot(pcl.T, pcl))
+def depth2pca(pnts, frame_id):
+    pnts = pnts.reshape(-1, 2).astype(np.float64)
+    mv = np.mean(pnts, 0).reshape(2, 1)
+    pnts -= mv.T
+    w, v = LA.eig(np.dot(pnts.T, pnts))
     w_max = np.max(w)
-    w_min = np.min(w)
 
     col = np.where(w == w_max)[0]
     if len(col) > 1:
         col = col[-1]
     V_max = v[:, col]
+    theta = math.atan(v_max[1], v_max[0]) / math.pi * 180.0
 
-    col_min = np.where(w == w_min)[0]
-    if len(col_min) > 1:
-        col_min = col_min[-1]
-    V_min = v[:, col_min]
+    pose = PoseStamped()
+    pose.pose.position.x = mv[0]
+    pose.pose.position.y = mv[1]
+    pose.pose.position.z = 0.0 
+    pose.pose.orientation.x = math.cos(theta/2.0) + math.sin(theta/2.0)
+    pose.pose.orientation.y = math.sin(theta/2.0) - math.cos(theta/2.0)
+    pose.pose.orientation.z = math.cos(theta/2.0) + math.sin(theta/2.0)
+    pose.pose.orientation.w = math.cos(theta/2.0) - math.sin(theta/2.0)
+    
+    return pose
 
-    return V_max, V_min, w_max, w_min
-
-def get_grasp(dm, thresh):
+def get_grasp_score(dm, thresh):
     v = abs(np.amin(dm)) - thresh
     if v < 0:
         return 0.0
@@ -69,24 +71,20 @@ def get_grasp(dm, thresh):
 if __name__ == '__main__':
     rospy.init_node('gelsight_proc')
 
+    frame_id = rospy.get_param('~frame_id', 'map')
+    rate = rospy.Rate(rospy.get_param('~rate', 30))
+    
     use_http = rospy.get_param('~use_http', True)
     cam_url = rospy.get_param('~cam_url', '')
-
     if use_http and cam_url == '':
         rospy.logfatal('No "cam_url" provided when "use_http" is true')
 
-    if not use_http: 
-        image_sub = rospy.Subscriber('/image/raw', Image, image_cb, queue_size=1)
-    else:
-        dev = gsdevice.Camera(gsdevice.Finger.R15, cam_url)
-        dev.connect()
-
-    pcl_pub = rospy.Publisher('/pcl', PointCloud2, queue_size=1)
-    contact_pub = rospy.Publisher('/contact', PoseStamped)
-    grasp_pub = rospy.Publisher('/grasped', Float32)
-
     width = rospy.get_param('~image_size/width')
     height = rospy.get_param('~image_size/height')
+    roi = (rospy.get_param('~image_roi/top_left/x'),
+           rospy.get_param('~image_roi/top_left/y'),
+           rospy.get_param('~image_roi/bottom_right/x'),
+           rospy.get_param('~image_roi/bottom_right/y'))
 
     grasp_thresh = rospy.get_param('~grasp_thresh', 4.7)
     depth_max = rospy.get_param('~depth_thresh/max')
@@ -95,45 +93,45 @@ if __name__ == '__main__':
     nn_path = rospy.get_param('~nn_path')
     nn_compute = rospy.get_param('~nn_compute', 'gpu')
 
+    pcl_pub = rospy.Publisher('/pcl', PointCloud2, queue_size=1)
+    contact_pub = rospy.Publisher('/contact', PoseStamped)
+    grasp_pub = rospy.Publisher('/grasped', Float32)
+
+    if not use_http: 
+        image_sub = rospy.Subscriber('/image/raw', Image, image_cb, queue_size=1)
+    else:
+        dev = gsdevice.Camera(gsdevice.Finger.R15, cam_url)
+        dev.connect()
+
     nn = gs3drecon.Reconstruction3D(gs3drecon.Finger.R15)
     nn.load_nn(nn_path, nn_compute)
 
-    #vis3d = gs3drecon.Visualize3D(120, 160, '', 0.0887)
-
     init_dm = None
-
-    rate = rospy.Rate(30)
     while not rospy.is_shutdown():
         try:
             if use_http and dev.while_condition:
-                last_frame = dev.get_image((60, 100, 375, 380))
+                last_frame = dev.get_image(roi)
 
             if last_frame is not None: 
                 frame = last_frame.copy()
+                
                 dm = nn.get_depthmap(frame, False)
                 dm *= -1
                 if init_dm is None:
                     init_dm = dm  
-
+		
                 dm = dm - init_dm 
                 dm[dm < depth_min] = 0.0
                 dm[dm > depth_max] = 0.0
 
-                pcl = depth2pcl(120, 160, 0.0887, dm)
-                v_max, v_min, wx, wy = pca(dm)
-                m = np.mean(dm, 0).reshape(-1)
-                pose = PoseStamped()
-                print(m)
-                pose.header.frame_id = "map"
-                pose.pose.position.x = m[0]
-                pose.pose.position.y = m[1]
-                pose.pose.orientation.w = 1
-                # pose.theta = math.atan2(v_max[1], v_max[0]) / math.pi * 180
-
+                pose = depth2pca(dm)
+                pose.header.frame_id = frame_id
                 contact_pub.publish(pose)
+
+                pcl = depth2pcl(120, 160, 0.0887, dm)
+                pcl.header.frame_id = frame_id
                 pcl_pub.publish(pcl)
                 grasp_pub.publish(Float32(get_grasp(dm, grasp_thresh)))
-                #vis3d.update(dm)
                     
             rate.sleep()
         except rospy.ROSInterruptException:
