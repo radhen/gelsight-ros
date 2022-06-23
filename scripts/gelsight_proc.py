b54640649134a8a9261d62efc72ef97a6e3eb592
Harrison McCarty <hmccarty@pm.me>
2022-06-23 01:22:48 -0400

    added marker flow

--- scripts/gelsight_proc.py
+++ scripts/gelsight_proc.py
@@ -14,168 +14,77 @@ from sensor_msgs.msg import Image, PointCloud2, PointField
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
 from cv_bridge import CvBridge
+from gelsight_ros.msg import MarkerFlow
 
 from gelsight import gsdevice
 from gelsight import gs3drecon
+
+from gelsight_util import *
+
 
 class ThreshType(Enum):
     GAUSSIAN = 1
     EXPONENTIAL = 2
 
+
 last_frame = None
 bridge = CvBridge()
 
+
 def image_cb(msg):
     global last_frame
-    last_frame = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-
-def depth2pcl(width, length, mmpp, dm):
-    points = []
-    for i in range(width):
-        for j in range(length):
-            points.append((i*mmpp/100.0, j*mmpp/100.0, dm[j, i]/1000.0, int(0)))
+    last_frame = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 
-    fields = [PointField('x', 0, PointField.FLOAT32, 1),
-              PointField('y', 4, PointField.FLOAT32, 1),
-              PointField('z', 8, PointField.FLOAT32, 1),
-              PointField('rgb', 12, PointField.UINT32, 1)]
+if __name__ == "__main__":
+    rospy.init_node("gelsight_proc")
 
-    header = Header()
-    return point_cloud2.create_cloud(header, fields, points)
+    frame_id = rospy.get_param("~frame_id", "map")
+    rate = rospy.Rate(rospy.get_param("~rate", 30))
 
-def depth2pca(dm, mmpp, buffer):
-    pnts = np.where(dm > 0)
-    X = pnts[1].reshape(-1, 1)
-    Y = pnts[0].reshape(-1, 1)
-    pnts = np.concatenate([X, Y], axis=1)
-    pnts = pnts.reshape(-1, 2).astype(np.float64) 
-    if pnts.shape[0] == 0 :
-        return None
-    
-    mv = np.mean(pnts, 0).reshape(2, 1)
-    pnts -= mv.T
-    w, v = LA.eig(np.dot(pnts.T, pnts))
-    w_max = np.max(w)
-
-    col = np.where(w == w_max)[0]
-    if len(col) > 1:
-        col = col[-1]
-     
-    V_max = v[:, col]
-    if V_max[0] > 0 and V_max[1] > 0:
-        V_max *= -1
-    
-    V_max = V_max.reshape(-1) * (w_max ** 0.3 / 1)
-    theta = math.atan2(V_max[1], V_max[0]) 
-
-    if len(buffer) > 0:
-        buffer.popleft() 
-    buffer.append((mv[0], mv[1], theta))
-
-    x_bar = 0.0
-    y_bar = 0.0
-    theta_bar = 0.0
-    for a in list(buffer):
-      x, y, theta = a
-      x_bar += x
-      y_bar += y
-      theta_bar += theta
-
-    if len(buffer) > 0:
-      x_bar /= len(buffer)
-      y_bar /= len(buffer)
-      theta_bar /= len(buffer) 
-
-    pose = PoseStamped()
-    pose.pose.position.x = x_bar*mmpp/100
-    pose.pose.position.y = y_bar*mmpp/100
-    pose.pose.position.z = 0.0 
-    
-    x, y, w, z = quaternion_from_euler(0.0, 0.0, theta_bar)
-    pose.pose.orientation.x = x
-    pose.pose.orientation.y = y
-    pose.pose.orientation.z = w
-    pose.pose.orientation.w = z
-    
-    return pose
-
-def get_2d_gaussian(n, m, sig):
-    x = np.linspace(-sig, sig, n)
-    x_gauss = np.exp(-0.5 * np.square(x) / np.square(sig))
-
-    y = np.linspace(-sig, sig, m)
-    y_gauss = np.exp(-0.5 * np.square(y) / np.square(sig))
-
-    return np.outer(x_gauss, y_gauss)
-
-def get_2d_exponential(n, m, beta):
-    x = np.linspace(0.0, beta, n//2) 
-    if n%2 == 0:
-        x = np.concatenate([x, np.linspace(beta, 0.0, n//2)])
-    else:
-        x = np.concatenate([x, np.linspace(beta, 0.0, (n//2)+1)])
-
-    y = np.linspace(0.0, beta, m//2) 
-    if m%2 == 0:
-        y = np.concatenate([y, np.linspace(beta, 0.0, m//2)])
-    else:
-        y = np.concatenate([y, np.linspace(beta, 0.0, (m//2)+1)])
-    
-    return np.outer(np.exp(x), np.exp(y))
-
-def get_grasp_score(dm, thresh):
-    v = abs(np.amin(dm)) - thresh
-    if v < 0:
-        return 0.0
-    elif v > 1:
-        return 1.0
-    return v
-
-if __name__ == '__main__':
-    rospy.init_node('gelsight_proc')
-
-    frame_id = rospy.get_param('~frame_id', 'map')
-    rate = rospy.Rate(rospy.get_param('~rate', 30))
-    
-    use_http = rospy.get_param('~use_http', True)
-    cam_url = rospy.get_param('~cam_url', '')
-    if use_http and cam_url == '':
+    use_http = rospy.get_param("~use_http", True)
+    cam_url = rospy.get_param("~cam_url", "")
+    if use_http and cam_url == "":
         rospy.logfatal('No "cam_url" provided when "use_http" is true')
 
-    width = rospy.get_param('~image_size/width')
-    height = rospy.get_param('~image_size/height')
-    roi = (rospy.get_param('~image_roi/top_left/x'),
-           rospy.get_param('~image_roi/top_left/y'),
-           rospy.get_param('~image_roi/bottom_right/x'),
-           rospy.get_param('~image_roi/bottom_right/y'))
+    width = rospy.get_param("~image_size/width")
+    height = rospy.get_param("~image_size/height")
+    roi = (
+        rospy.get_param("~image_roi/top_left/x"),
+        rospy.get_param("~image_roi/top_left/y"),
+        rospy.get_param("~image_roi/bottom_right/x"),
+        rospy.get_param("~image_roi/bottom_right/y"),
+    )
 
-    depth_thresh_type = rospy.get_param('~depth_thresh/type', None) 
-    gauss_params = rospy.get_param('~depth_thresh/gaussian', None)
-    exp_params = rospy.get_param('~depth_thresh/exponential', None)
+    depth_thresh_type = rospy.get_param("~depth_thresh/type", None)
+    gauss_params = rospy.get_param("~depth_thresh/gaussian", None)
+    exp_params = rospy.get_param("~depth_thresh/exponential", None)
     if depth_thresh_type is not None:
         if depth_thresh_type == "gaussian":
             depth_thresh_type = ThreshType.GAUSSIAN
         elif depth_thresh_type == "exponential":
             depth_thresh_type = ThreshType.EXPONENTIAL
         else:
-            rospy.logfatal(f'Depth thresh type not recognized: {depth_thresh_type}')
+            rospy.logfatal(f"Depth thresh type not recognized: {depth_thresh_type}")
 
-    nn_path = rospy.get_param('~nn_path')
-    nn_compute = rospy.get_param('~nn_compute', 'gpu')
-    nn_output_width = rospy.get_param('~nn_output_size/width')
-    nn_output_length = rospy.get_param('~nn_output_size/height')
-    nn_mmpp = rospy.get_param('~nn_mmpp')
+    nn_path = rospy.get_param("~nn_path")
+    nn_compute = rospy.get_param("~nn_compute", "gpu")
+    nn_output_width = rospy.get_param("~nn_output_size/width")
+    nn_output_length = rospy.get_param("~nn_output_size/height")
+    nn_mmpp = rospy.get_param("~nn_mmpp")
 
-    gaussian_width = rospy.get_param('~gaussian_kernel/width')
-    gaussian_height = rospy.get_param('~gaussian_kernel/height')
+    publish_markers = rospy.get_param("~publish_markers", False)
 
-    pcl_pub = rospy.Publisher('/pcl', PointCloud2, queue_size=1)
-    contact_pub = rospy.Publisher('/contact', PoseStamped)
-    grasp_pub = rospy.Publisher('/grasped', Float32)
+    gaussian_width = rospy.get_param("~gaussian_kernel/width")
+    gaussian_height = rospy.get_param("~gaussian_kernel/height")
 
-    if not use_http: 
-        image_sub = rospy.Subscriber('/image/raw', Image, image_cb, queue_size=1)
+    pcl_pub = rospy.Publisher("/pcl", PointCloud2, queue_size=1)
+    contact_pub = rospy.Publisher("/contact", PoseStamped)
+    grasp_pub = rospy.Publisher("/grasped", Float32)
+    marker_flow_pub = rospy.Publisher("/flow", MarkerFlow)
+
+    if not use_http:
+        image_sub = rospy.Subscriber("/image/raw", Image, image_cb, queue_size=1)
     else:
         dev = gsdevice.Camera(gsdevice.Finger.R15, cam_url)
         dev.connect()
@@ -183,23 +92,35 @@ if __name__ == '__main__':
     nn = gs3drecon.Reconstruction3D(gs3drecon.Finger.R15)
     nn.load_nn(nn_path, nn_compute)
 
+    init_frame = None
+    init_markers = None
     init_dm = None
-    pca_buffer = deque([], maxlen=rospy.get_param('~pca_buffer_size'))
+    pca_buffer = deque([], maxlen=rospy.get_param("~pca_buffer_size"))
     while not rospy.is_shutdown():
         try:
             if use_http and dev.while_condition:
                 last_frame = dev.get_image(roi)
 
-            if last_frame is not None: 
+            if last_frame is not None:
                 frame = last_frame.copy()
-                
+
+                if init_frame is None:
+                    init_frame = frame
+
+                if publish_markers:
+                    if init_markers is None:
+                        init_markers = image2markers(frame)
+                    else:
+                        flow_msg = image2flow(init_markers, init_frame, frame)
+                        marker_flow_pub.publish(flow_msg)
+
                 dm = nn.get_depthmap(frame, False)
 
                 dm *= -1
                 if init_dm is None:
-                    init_dm = dm  
+                    init_dm = dm
 
-                dm = dm - init_dm 
+                dm = dm - init_dm
 
                 pcl = depth2pcl(nn_output_width, nn_output_length, nn_mmpp, dm)
                 pcl.header.frame_id = frame_id
@@ -207,15 +128,19 @@ if __name__ == '__main__':
 
                 if depth_thresh_type is not None:
                     if depth_thresh_type == ThreshType.GAUSSIAN:
-                        gauss = get_2d_gaussian(dm.shape[0], dm.shape[1], gauss_params['sig'])
+                        gauss = get_2d_gaussian(
+                            dm.shape[0], dm.shape[1], gauss_params["sig"]
+                        )
                         thresh = dm - gauss
-                        dm[thresh > gauss_params['max']] = 0.0
-                        dm[thresh < gauss_params['min']] = 0.0
+                        dm[thresh > gauss_params["max"]] = 0.0
+                        dm[thresh < gauss_params["min"]] = 0.0
                     elif depth_thresh_type == ThreshType.EXPONENTIAL:
-                        exp = get_2d_exponential(dm.shape[0], dm.shape[1], exp_params['beta'])
+                        exp = get_2d_exponential(
+                            dm.shape[0], dm.shape[1], exp_params["beta"]
+                        )
                         thresh = dm - exp
-                        dm[thresh > exp_params['max']] = 0.0
-                        dm[thresh < exp_params['min']] = 0.0
+                        dm[thresh > exp_params["max"]] = 0.0
+                        dm[thresh < exp_params["min"]] = 0.0
 
                 pose = depth2pca(dm, nn_mmpp, pca_buffer)
                 if pose is not None:
---
+++ scripts/gelsight_util.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import cv2
+from scipy.signal import fftconvolve
+from scipy.ndimage.filters import maximum_filter, minimum_filter
+from math import sqrt
+import numpy as np
+from gelsight_ros.msg import MarkerFlow
+from geometry_msgs.msg import Vector3
+
+MARKER_INTENSITY_SCALE = 3
+MARKER_THRESHOLD = 255
+MARKER_TEMPLATE_SIZE = 20
+MARKER_TEMPLATE_RADIUS = 5
+MARKER_NEIGHBORHOOD_SIZE = 20
+
+
+def image2markers(image):
+    # Mask markers
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    scaled = cv2.convertScaleAbs(gray, alpha=MARKER_INTENSITY_SCALE)
+    mask = cv2.inRange(scaled, MARKER_THRESHOLD, MARKER_THRESHOLD)
+
+    # Perform normalized cross-correlation with gaussian kernel
+    # See https://github.com/gelsightinc/gsrobotics/blob/main/examples/markertracking.py
+    t = get_2d_gaussian(
+        MARKER_TEMPLATE_SIZE, MARKER_TEMPLATE_SIZE, MARKER_TEMPLATE_RADIUS
+    )
+    t = t - np.mean(t)
+    a = mask - np.mean(mask)
+    n_xcorr = fftconvolve(a, np.flipud(np.fliplr(t)).conj(), mode="same")
+    a = fftconvolve(np.square(a), np.ones(t.shape), mode="same") - np.square(
+        fftconvolve(a, np.ones(t.shape), mode="same") / np.prod(t.shape)
+    )
+    a[np.where(a < 0)] = 0
+    t = np.sum(np.square(t))
+    n_xcorr = n_xcorr / np.sqrt(a * t)
+    n_xcorr[np.where(np.logical_not(np.isfinite(a)))] = 0
+
+    # Dilate image
+    dilated = cv2.dilate(mask, np.ones((3, 3), np.uint8), iterations=1)
+    b = 2 * ((n_xcorr - n_xcorr.min()) / (n_xcorr.max() - n_xcorr.min())) - 1
+    b = (b - b.min()) / (b.max() - b.min())
+    mask = np.asarray(b < 0.5)
+    mask = (mask * 255).astype("uint8")
+
+    # Find peaks
+    max = maximum_filter(mask, MARKER_NEIGHBORHOOD_SIZE)
+    maxima = mask == max
+    min = minimum_filter(mask, MARKER_NEIGHBORHOOD_SIZE)
+    diff = (max - min) > threshold
+    maxima[diff == 0] = 0
+
+    labeled, n = ndimage.label(maxima)
+    xy = np.array(ndimage.center_of_mass(mask, labeled, range(1, n + 1)))
+    xy[:, [0, 1]] = xy[:, [1, 0]]
+    return xy
+
+
+def image2flow(init_markers, init_image, image):
+    pts, _, _ = cv2.calcOpticalFlowPyrLK(
+        init_image,
+        image,
+        init_markers.astype("float32"),
+        None,
+        winSize=(100, 100),
+        maxLevel=2,
+        criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 2),
+        flags=0,
+    )
+
+    flow_msg = MarkerFlow()
+    flow_msg.data = []
+    for pt in pts:
+        flow_msg.data.append(Vector3(x=pt[0], y=[1]))
+
+    return flow_msg
+
+
+def depth2pcl(width, length, mmpp, dm):
+    points = []
+    for i in range(width):
+        for j in range(length):
+            points.append(
+                (i * mmpp / 100.0, j * mmpp / 100.0, dm[j, i] / 1000.0, int(0))
+            )
+
+    fields = [
+        PointField("x", 0, PointField.FLOAT32, 1),
+        PointField("y", 4, PointField.FLOAT32, 1),
+        PointField("z", 8, PointField.FLOAT32, 1),
+        PointField("rgb", 12, PointField.UINT32, 1),
+    ]
+
+    header = Header()
+    return point_cloud2.create_cloud(header, fields, points)
+
+
+def depth2pca(dm, mmpp, buffer):
+    pnts = np.where(dm > 0)
+    X = pnts[1].reshape(-1, 1)
+    Y = pnts[0].reshape(-1, 1)
+    pnts = np.concatenate([X, Y], axis=1)
+    pnts = pnts.reshape(-1, 2).astype(np.float64)
+    if pnts.shape[0] == 0:
+        return None
+
+    mv = np.mean(pnts, 0).reshape(2, 1)
+    pnts -= mv.T
+    w, v = LA.eig(np.dot(pnts.T, pnts))
+    w_max = np.max(w)
+
+    col = np.where(w == w_max)[0]
+    if len(col) > 1:
+        col = col[-1]
+
+    V_max = v[:, col]
+    if V_max[0] > 0 and V_max[1] > 0:
+        V_max *= -1
+
+    V_max = V_max.reshape(-1) * (w_max**0.3 / 1)
+    theta = math.atan2(V_max[1], V_max[0])
+
+    if len(buffer) > 0:
+        buffer.popleft()
+    buffer.append((mv[0], mv[1], theta))
+
+    x_bar = 0.0
+    y_bar = 0.0
+    theta_bar = 0.0
+    for a in list(buffer):
+        x, y, theta = a
+        x_bar += x
+        y_bar += y
+        theta_bar += theta
+
+    if len(buffer) > 0:
+        x_bar /= len(buffer)
+        y_bar /= len(buffer)
+        theta_bar /= len(buffer)
+
+    pose = PoseStamped()
+    pose.pose.position.x = x_bar * mmpp / 100
+    pose.pose.position.y = y_bar * mmpp / 100
+    pose.pose.position.z = 0.0
+
+    x, y, w, z = quaternion_from_euler(0.0, 0.0, theta_bar)
+    pose.pose.orientation.x = x
+    pose.pose.orientation.y = y
+    pose.pose.orientation.z = w
+    pose.pose.orientation.w = z
+
+    return pose
+
+
+def get_2d_gaussian(n, m, sig):
+    x = np.linspace(-(n - 1) / 2.0, (n - 1) / 2.0, n)
+    x_gauss = np.exp(-0.5 * np.square(x) / np.square(sig))
+
+    y = np.linspace(-(m - 1) / 2.0, (m - 1) / 2.0, m)
+    y_gauss = np.exp(-0.5 * np.square(y) / np.square(sig))
+
+    gauss = np.outer(x_gauss, y_gauss)
+    return gauss / np.sum(gauss)
+
+
+def get_2d_exponential(n, m, beta):
+    x = np.linspace(0.0, beta, n // 2)
+    if n % 2 == 0:
+        x = np.concatenate([x, np.linspace(beta, 0.0, n // 2)])
+    else:
+        x = np.concatenate([x, np.linspace(beta, 0.0, (n // 2) + 1)])
+
+    y = np.linspace(0.0, beta, m // 2)
+    if m % 2 == 0:
+        y = np.concatenate([y, np.linspace(beta, 0.0, m // 2)])
+    else:
+        y = np.concatenate([y, np.linspace(beta, 0.0, (m // 2) + 1)])
+
+    return np.outer(np.exp(x), np.exp(y))
+
+
+def get_grasp_score(dm, thresh):
+    v = abs(np.amin(dm)) - thresh
+    if v < 0:
+        return 0.0
+    elif v > 1:
+        return 1.0
+    return v
