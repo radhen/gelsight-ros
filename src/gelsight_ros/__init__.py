a5824f045b7d0c7f0a43aa508cb7c6e1ef9bf30d
Harrison McCarty <hmccarty@pm.me>
2022-06-30 03:13:26 -0400

    Revert "refactored main pipeline"
    This reverts commit 30ce7371e0d7f28946338b980f63322241181085.

--- src/gelsight_ros/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-from .stream import GelsightStream, GelsightFileStream, GelsightHTTPStream
-from .proc import GelsightProc, MarkersProc, FlowProc, DepthProc, PoseFromDepthProc
