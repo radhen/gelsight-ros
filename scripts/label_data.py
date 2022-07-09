300e069d3b7d0a1dbbb03a11638dd299f6c38001
Harrison McCarty <hmccarty@pm.me>
2022-07-09 16:43:10 -0400

    added changes for mvp

--- scripts/label_data.py
+++ scripts/label_data.py
@@ -21,60 +21,19 @@ You can train a new model from the output dataset using the 'train.py' script.
 
 import csv
 from csv import writer
+import cv2
 import gelsight_ros as gsr
 import math
-from os import listdir
-from os.path import isfile, join
+import numpy as np
+import os
+import rospy
 
 DEFAULT_RATE = 30
 DEFAULT_FIELD_NAMES = ['R', 'G', 'B', 'x', 'y', 'gx', 'gy']
-
 mpp = 0.08
-radius = None
-circle = None
-current_frame = None
-output_file = None
-
-def save_cb(state):
-    global mpp, radius, circle, current_frame, output_file
-
-    if not mpp:
-        rospy.logfatal("Saved without meter-per-pixel. Exiting.")
-    if not radius:
-        rospy.logfatal("Saved without radius. Exiting.")
-    if not circle:
-        rospy.logfatal("Saved without circle. Exiting.")
-    elif not current_frame:
-        rospy.logfatal("Saved without current frame. Exiting.")
-    elif not output_file:
-        rospy.logfatal("Saved without output file. Exiting.")
-
-    labels = []
-    for x in range(current_frame.shape[0]):
-        for y in range(current_frame.shape[1]):
-
-            dx = (x - circle[0]) * mpp
-            dy = (y - circle[1]) * mpp
-
-            gx = (-dx) / math.sqrt(radius**2 - dx**2 - dy**2)
-            gy = (-dy) / math.sqrt(radius**2 - dx**2 - dy**2)
-
-            labels.append((current_frame[x, y, 0],
-                           current_frame[x, y, 1],
-                           current_frame[x, y, 2],
-                           x, y, gx, gy))
-
-    with open(output_file, 'a', newline='') as f:
-        w = writer(f)
-        for label in labels:
-            w.writerow(label)
-        w.close()
-
-    current_frame = None
 
 if __name__ == "__main__":
     rospy.init_node("label")
-    rate = rospy.Rate(rospy.get_param("~rate", DEFAULT_RATE))
 
     # Retrieve path where images are saved
     if not rospy.has_param("~input_path"):
@@ -98,39 +57,84 @@ if __name__ == "__main__":
             rospy.signal_shutdown(f"Failed to create output folder: {output_path}")
     output_file = output_path + "/gelsight-depth-dataset.csv"
 
+    with open(output_file, 'w', newline='') as f:
+        w = writer(f)
+        w.writerow(DEFAULT_FIELD_NAMES)
+
     # Retrieve ball radius for building labels
     if not rospy.has_param("~ball_radius"):
         rospy.signal_shutdown("No ball radius provided. Please set ball_radius.")
     radius = rospy.get_param("~ball_radius")
 
     # Retrieve all images from input path
-    imgs = [f for f in listdir(input_path) if isfile(join(input_path, f))]
-    init_frame = imgs[0]
+    imgs = [input_path + "/" + f for f in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, f))]
+    init_frame = cv2.imread(imgs[0])
+    imgs = imgs[1:]                
 
     # Configure cv window
     cv2.namedWindow('label_data')
-    cv2.createButton('Save', save_cb, None, cv2.QT_PUSH_BUTTON, 1)
+
 
     # Main loop
     while not rospy.is_shutdown() and len(imgs) > 0:
         try:
-            if not current_frame:
-                imgs = imgs[1:]                
-                current_frame = cv2.imread(imgs[0], cv2.IMREAD_RGB)
+            # Collect next frame and compute diff from initial frame
+            current_frame = ((cv2.imread(imgs[0]) * 1.0) - init_frame) * 4 + 127
+            current_frame[current_frame > 255] = 255
+            current_frame[current_frame < 0] = 0
+            current_frame = np.uint8(current_frame)
 
-            circles = cv.HoughCircles(current_frame, cv.HOUGH_GRADIENT, 1, 20,
-                param1=50,param2=30,minRadius=0,maxRadius=0)
+            # Convert to grayscale and find circles using hough transform
+            grayscale_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2GRAY)
+            circles = cv2.HoughCircles(grayscale_frame, cv2.HOUGH_GRADIENT, 1, 20,
+                param1=50,param2=30,minRadius=0,maxRadius=100)
 
             display_frame = current_frame.copy()
-            circle = np.uint16(np.around(circles))[0]
-            cv.circle(display_frame, (circle[0], circle[1]), circle[2], (0,255,0), 2)
-            cv.circle(display_frame, (circle[0], circle[1]), 2,(0,0,255), 3)
+            if len(circles) > 0:
+                # Draw first circle
+                circle = np.uint16(np.around(circles))[0,:,0]
+                cv2.circle(display_frame, (int(circle[0]), int(circle[1])),
+                    int(circle[2]), (0,255,0), 2)
+                cv2.circle(display_frame, (int(circle[0]), int(circle[1])),
+                    2, (0, 0, 255), 3)
 
             cv2.imshow('label_data', display_frame) 
-            cv2.waitKey(0)
             
-            rate.sleep()
+            k = cv2.waitKey(0)
+            if k == ord('y'):
+                # Compute labels from circle
+                labels = []
+                for x in range(current_frame.shape[0]):
+                    for y in range(current_frame.shape[1]):
+
+                        dx = (x - circle[0]) * mpp
+                        dy = (y - circle[1]) * mpp
+
+                        dist = radius**2 - dx**2 - dy**2
+                        if dist <= 0.0 or dist > radius:
+                            continue
+
+                        r = current_frame[x, y, 0]
+                        g = current_frame[x, y, 1]
+                        b = current_frame[x, y, 2]
+
+                        gx = (-dx) / math.sqrt(dist)
+                        gy = (-dy) / math.sqrt(dist)
+
+                        labels.append((r, g, b, x, y, gx, gy))
+
+                # Write all labels to CSV file 
+                with open(output_file, 'a', newline='') as f:
+                    w = writer(f)
+                    for label in labels:
+                        w.writerow(label)
+            elif k == ord('q'):
+                break            
+
+            # Move to next image
+            imgs = imgs[1:]
         except rospy.ROSInterruptException:
             break
 
     cv2.destroyAllWindows()
+    rospy.signal_shutdown(f"Data labeling completed, output to {output_file}")
